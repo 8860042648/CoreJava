768fdf517c8564a6ee279b7e26c7674c5c208840
8860042648 <114214414+8860042648@users.noreply.github.com>
2022-10-05 14:02:44 +0530

    Add files via upload

---
+++ Lab Session/29-09-2022/Child.java
@@ -0,0 +1,22 @@
+package com.java;
+
+//  child class which inherit property from  parent class.
+
+      class Child extends Parent 
+{
+	// Speak method .
+	
+     void Speak()
+ {
+System.out.println("I am Speaking .")	; 
+ }
+ 
+ // This is dance method.
+ 
+          void Dance()
+ {
+ System.out.println("I am dancing .");
+	 
+ }
+ 
+}
---
+++ Lab Session/29-09-2022/Parent.java
@@ -0,0 +1,23 @@
+package com.java;
+
+// Here we create parent class.
+   
+class Parent {
+	      void parent() 
+	{
+		
+		//This is a constructor.
+		
+		System.out.println("Hello  I am your parents.");
+		
+	}
+	
+	//And This is a Sing method.
+	
+        void Sing() 
+    {
+    	System.out.println(" Singing a song.");
+    }
+    
+
+}
