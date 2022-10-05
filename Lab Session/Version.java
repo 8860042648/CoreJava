662ead44836f5a27e2d91fedb67bdb2e86aa11a8
8860042648 <114214414+8860042648@users.noreply.github.com>
2022-10-05 13:30:03 +0530

    Add files via upload

---
+++ Lab Session/Version.java
@@ -0,0 +1,24 @@
+package com.java;
+
+/* Program:-Find the version of Java.
+ * 
+ * @author:-Pooja Gautam
+ * @date:-01-10-2022
+ */
+
+
+// Main  class
+
+ class Version 
+ {
+	 
+  public static void main(String[] args)
+  
+  //Here we Print of java version.
+  
+    {
+         System.out.println("version of Java is: "+ System.getProperty("java.version"));	
+    }   
+	
+
+}
